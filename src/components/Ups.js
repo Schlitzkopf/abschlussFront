08225a8fe82acf0f4dc6ec9a68b62b93b01ac475
Ferdinand <holdenriederferdinand@gmail.com>
2022-02-02 12:43:43 +0100

    Ups seite Bild optimiert

--- src/components/Ups.js
+++ src/components/Ups.js
@@ -4,7 +4,7 @@ const Ups = () => {
   return (
     <div className="text-center">
       <h2>Sry we are working on this link.</h2>
-      <img src="https://acegif.com/wp-content/uploads/cat-typing-16.gif" alt="" />
+      <img className="img-fluid" src="https://acegif.com/wp-content/uploads/cat-typing-16.gif" alt="" />
     </div>
   );
 };
