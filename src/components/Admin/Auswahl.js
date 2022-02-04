e986f8a057bfe53d8e1a34f1327a0017185a478b
Ferdinand Surface <holdenriederferdinand@gmail.com>
2022-02-04 19:27:56 +0100

    h

--- src/components/Admin/Auswahl.js
+++ src/components/Admin/Auswahl.js
@@ -167,7 +167,7 @@ const Auswahl = () => {
         </div>
       ) : (
         <div>
-          <div className="text-center">
+          <div className="text-center text-light">
             <h2>Daten werden verarbeitet...</h2>
             <img
               src="https://media3.giphy.com/media/Oc8lIQHZsXqDu/200w.gif"
