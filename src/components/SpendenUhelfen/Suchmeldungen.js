e58e1d2e4a750a3d32d9e41dbbecb6ae4933f27f
Ferdinand <holdenriederferdinand@gmail.com>
2022-02-08 17:31:59 +0100

    Spenden und Helfen etwas gefüllt

--- src/components/SpendenUhelfen/Suchmeldungen.js
+++ src/components/SpendenUhelfen/Suchmeldungen.js
@@ -3,12 +3,54 @@ import { Link } from "react-router-dom";
 
 const Suchmeldungen = () => {
   return (
-    <div>
-      {" "}
-      <Link to={"/"} className="btn btn-secondary">
-        Zurück
-      </Link>
-      <h1>Hier kommen Suchmeldungen rein</h1>
+    <div className="container">
+      <div className="list-group d-grid gap-3 ">
+        <a
+          href="https://www.tasso.net/"
+          className="list-group-item list-group-item-action backgroundSchutz"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">TASSO</h5>
+          </div>
+          <p className="mb-1">
+            Zum Schutz von Tieren setzt sich TASSO engagiert dafür ein, jedes
+            entlaufene Tier so schnell wie möglich wieder zu seinem Halter
+            zurückzubringen. Darüber hinaus engagieren wir uns auch für alle
+            anderen Tiere in Not.
+          </p>
+        </a>
+        <a
+          href="https://www.lippewelle.de/index.html"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">Lippewelle</h5>
+          </div>
+          <p className="mb-1">
+            Lippewelle ist nicht nur ein Radiosender. Sie bieten auch eine
+            Tiersuchmeldung an.
+          </p>
+        </a>
+        <a
+          href="http://www.fellsuche.eu/index.php"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">fellsuche.eu</h5>
+          </div>
+          <p className="mb-1">
+            Selbstverständlich ist es auf jeden Fall von Nutzen, wenn Sie z.B.
+            Ihre Bekannten per e-mail oder per Telefonanruf über das Entlaufen
+            Ihres Lieblings in Kenntnis setzen. Auch hilft es mit Sicherheit,
+            wenn Sie dies über Twitter, Facebook und googlePlus veröffentlichen.
+            Alle diese Dinge sind auf jeden Fall wichtig, wir verstehen aus
+            eigener Erfahrung, dass Sie Ihr Haustier gerne zurück haben wollen!
+            Entschliessen Sie sich, Ihre Suchmeldung einmal hier bei uns
+            einzutragen, dann übernehmen wir das Verteilen für Sie, wir wissen,
+            dass Sie jetzt "andere Dinge im Kopf" haben.
+          </p>
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/SpendenUhelfen/Tierschutzhelfer.js
+++ src/components/SpendenUhelfen/Tierschutzhelfer.js
@@ -3,12 +3,95 @@ import { Link } from "react-router-dom";
 
 const Tierschutzhelfer = () => {
   return (
-    <div>
-      {" "}
-      <Link to={"/"} className="btn btn-secondary">
-        Zurück
-      </Link>
-      <h1>Hier kommen Tierschutzhelfer rein</h1>
+    <div className="container">
+      <div className="list-group d-grid gap-3 ">
+        <a
+          href="https://schnueffelfreunde.de/"
+          className="list-group-item list-group-item-action backgroundSchutz"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">Schnüffelfreunde</h5>
+          </div>
+          <p className="mb-1">
+            Liebe Hundefreunde, ich war selbst auf der Suche nach hochwertigen
+            Hundeartikeln und konnte oft nicht das Passende finden. Meist
+            stimmte die Qualität einfach nicht. Deshalb wurde Schnüffelfreunde
+            gegründet. Schnüffelfreunde ein kleiner Betrieb und dessen Ziel ist
+            es, hochwertige Produkte anzubieten, die sowohl Hund als auch Halter
+            Spaß machen. Ich wünsche viel Freude mit unseren Artikeln!
+          </p>
+        </a>
+        <a
+          href="https://www.tierheimleben-in-not.de/"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">Tierheimleben in Not e.V.</h5>
+          </div>
+          <p className="mb-1">
+            Der Verein Tierheimleben in Not wurde im Dezember 2011 gegründet.
+            TIN arbeitet in Ungarn und betreut dort mehrere Tierheime und eine
+            Tötungsstation. Im Juni 2012 bekam TIN bereits die Gemeinnützigkeit
+            anerkannt, im Juli 2014 wurde TIN beim Registergericht eingetragen
+            und erhielt den Zusatz e.V.
+          </p>
+        </a>
+        <a
+          href="https://www.streunerhilfe-ni-no.de/"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">Streuenerhilfe Ni-No e.V.</h5>
+          </div>
+          <p className="mb-1">
+            Wir sind ein noch junger Tierschutzverein, der sich um
+            Streunerkatzen und Katzen in Not kümmert. Unser Einzugsgebiet bewegt
+            sich im Groben an der Grenze Niedersachsen/Nordrhein-Westfalen
+            (Osnabrück, Melle, Rheine, Münster), doch wir möchten uns nicht
+            festlegen lassen. Wir helfen wo wir gebraucht werden und
+            Hilfestellung leisten können. Frei von festgelegten Grenzen.
+          </p>
+        </a>
+        <a
+          href="https://www.susyutzinger.ch/"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">Susy utzinger</h5>
+          </div>
+          <p className="mb-1">
+            Die Susy Utzinger Stiftung für Tierschutz ist ununterbrochen Aktiv.
+            Mit professioneller Planung und packenden Einsätzen vor Ort
+            unterstützt und fördert sie Tierheime im In-und Ausland. Diese
+            Tierschutzarbeit wird hauptsächlich durch Spenden und Legate
+            ermöglicht. Ein elementarer und unverzichtbarer Teil ist aber auch
+            die unentgeldliche Mitarbeit von Fachleuten. Die Einsatz-Teams der
+            SUST verfügen über ein grosses Herz für Tiere, enorm viel Fachwissen
+            und Erfahrungen im Tierschutzbereich und grenzenlosen Einsatzwillen:
+            Tierleid, Schmutz, Arbeit unter extremen Bedingungen und unter
+            Umständen auch Verletzungen gehören zum Alltag solcher Einsätze.
+          </p>
+        </a>
+        <a
+          href="https://www.natucate.com/"
+          className="list-group-item list-group-item-action"
+        >
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">NATUCATE</h5>
+          </div>
+          <p className="mb-1">
+            Leiste in diesem Freiwilligenprojekt einen aktiven Beitrag zum
+            Tierschutz und widme Dich der Versorgung streunender Hunde und
+            Katzen auf der Doppel-Insel Ko Lanta im Süden Thailands. Viele der
+            Tiere wurden von Arbeitern, die beim Wiederaufbau der Insel nach dem
+            verheerenden Tsunami im Jahr 2004 mitgeholfen haben, zurückgelassen.
+            Dieses Hilfsprojekt nimmt sich dem Schicksal hilfsbedürftiger Hunde
+            und Katzen an und bietet ihnen Schutz und Versorgung. Werde Teil
+            eines Teams aus freiwilligen Helfern und mache bereichernde
+            Erfahrungen in der täglichen Tierbetreuung.
+          </p>
+        </a>
+      </div>
     </div>
   );
 };
